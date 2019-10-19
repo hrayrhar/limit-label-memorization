Fix a bug in visualizing predicted gradients

modules/visualization.py
@@ -181,7 +181,7 @@ def latent_space_tsne(model, data_loader, plt=None):
     return fig, plt
 
 
-def ce_gradient_norm_histogram(model, data_loader, tensorboard, epoch, name, max_num_examples=10000):
+def ce_gradient_norm_histogram(model, data_loader, tensorboard, epoch, name, max_num_examples=5000):
     model.eval()
 
     pred = utils.apply_on_dataset(model=model, dataset=data_loader.dataset,
@@ -212,7 +212,7 @@ def ce_gradient_pair_scatter(model, data_loader, d1=0, d2=1, max_num_examples=20
     labels = utils.to_cpu(labels)
     grad_wrt_logits = torch.softmax(pred, dim=-1)[:max_num_examples] - labels[:max_num_examples]
 
-    fig, ax = plt.subplots(1)
+    fig, ax = plt.subplots(1, figsize=(5, 5))
     plt.scatter(grad_wrt_logits[:, d1], grad_wrt_logits[:, d2])
     ax.set_xlabel(str(d1))
     ax.set_ylabel(str(d2))
@@ -224,19 +224,12 @@ def ce_gradient_pair_scatter(model, data_loader, d1=0, d2=1, max_num_examples=20
     return fig, plt
 
 
-def pred_gradient_norm_histogram(model, data_loader, tensorboard, epoch, name, max_num_examples=10000):
+def pred_gradient_norm_histogram(model, data_loader, tensorboard, epoch, name, max_num_examples=5000):
     model.eval()
-
-    pred = utils.apply_on_dataset(model=model, dataset=data_loader.dataset,
-                                  output_keys_regexp='grad_pred', description='grad-histogram:grad_pred',
-                                  max_num_examples=max_num_examples)['grad_pred']
-    labels = [p[1] for p in data_loader.dataset]
-    labels = torch.tensor(labels, dtype=torch.long)
-    labels = F.one_hot(labels, num_classes=model.num_classes).float()
-    labels = utils.to_cpu(labels)
-
-    grad_wrt_logits = torch.softmax(pred, dim=-1)[:max_num_examples] - labels[:max_num_examples]
-    grad_norms = torch.sum(grad_wrt_logits**2, dim=-1)
+    grad_pred = utils.apply_on_dataset(model=model, dataset=data_loader.dataset,
+                                       output_keys_regexp='grad_pred', description='grad-histogram:grad_pred',
+                                       max_num_examples=max_num_examples)['grad_pred']
+    grad_norms = torch.sum(grad_pred**2, dim=-1)
     tensorboard.add_histogram(tag=name, values=grad_norms, global_step=epoch)
 
 
@@ -244,23 +237,17 @@ def pred_gradient_pair_scatter(model, data_loader, d1=0, d2=1, max_num_examples=
     if plt is None:
         plt = matplotlib.pyplot
     model.eval()
+    grad_pred = utils.apply_on_dataset(model=model, dataset=data_loader.dataset,
+                                       output_keys_regexp='grad_pred',
+                                       max_num_examples=max_num_examples,
+                                       description='grad-pair-scatter:grad_pred')['grad_pred']
 
-    pred = utils.apply_on_dataset(model=model, dataset=data_loader.dataset,
-                                  output_keys_regexp='grad_pred',
-                                  max_num_examples=max_num_examples,
-                                  description='grad-pair-scatter:grad_pred')['grad_pred']
-    labels = [p[1] for p in data_loader.dataset]
-    labels = torch.tensor(labels, dtype=torch.long)
-    labels = F.one_hot(labels, num_classes=model.num_classes).float()
-    labels = utils.to_cpu(labels)
-    grad_wrt_logits = torch.softmax(pred, dim=-1)[:max_num_examples] - labels[:max_num_examples]
-
-    fig, ax = plt.subplots(1)
-    plt.scatter(grad_wrt_logits[:, d1], grad_wrt_logits[:, d2])
+    fig, ax = plt.subplots(1, figsize=(5, 5))
+    plt.scatter(grad_pred[:, d1], grad_pred[:, d2])
     ax.set_xlabel(str(d1))
     ax.set_ylabel(str(d2))
-    # L = np.percentile(grad_wrt_logits, q=5, axis=0)
-    # R = np.percentile(grad_wrt_logits, q=95, axis=0)
+    # L = np.percentile(grad_pred, q=5, axis=0)
+    # R = np.percentile(grad_pred, q=95, axis=0)
     # ax.set_xlim(L[d1], R[d1])
     # ax.set_ylim(L[d2], R[d2])
     ax.set_title('Two coordinates of grad wrt to logits')