d61b7be181c37b56cf880d055b59fc3a6b38e7ee
Hrayr Harutyunyan <harhro@gmail.com>
2020-01-09 13:33:05 -0800

    Try expect in extracting the results

--- scripts/extract_results_from_logs.py
+++ scripts/extract_results_from_logs.py
@@ -17,31 +17,35 @@ def main():
     for instance in tqdm(logs):
         if instance == '.gitkeep':
             continue
+        try:
+            args_file = os.path.join(args.log_dir, instance, 'args.pkl')
+            if not os.path.exists(args_file):
+                print("===> args.pkl is missing: {}".format(instance))
+                continue
+            with open(args_file, 'rb') as f:
+                result = vars(pickle.load(f))
 
-        args_file = os.path.join(args.log_dir, instance, 'args.pkl')
-        if not os.path.exists(args_file):
-            print("===> args.pkl is missing: {}".format(instance))
-            continue
-        with open(args_file, 'rb') as f:
-            result = vars(pickle.load(f))
+            test_file = os.path.join(args.log_dir, instance, 'test_accuracy.txt')
+            if not os.path.exists(test_file):
+                print("===> test_accuracy.txt is missing: {}".format(instance))
+                continue
+            with open(test_file, 'r') as f:
+                test_acc = float(f.read())
+                result['test_accuracy'] = test_acc
 
-        test_file = os.path.join(args.log_dir, instance, 'test_accuracy.txt')
-        if not os.path.exists(test_file):
-            print("===> test_accuracy.txt is missing: {}".format(instance))
-            continue
-        with open(test_file, 'r') as f:
-            test_acc = float(f.read())
-            result['test_accuracy'] = test_acc
+            val_result_file_path = os.path.join(args.log_dir, instance, 'best_val_result.txt')
+            if os.path.exists(val_result_file_path):
+                with open(val_result_file_path, 'r') as f:
+                    val_acc = float(f.read())
+            else:
+                val_acc = -1.0
+            result['val_accuracy'] = val_acc
 
-        val_result_file_path = os.path.join(args.log_dir, instance, 'best_val_result.txt')
-        if os.path.exists(val_result_file_path):
-            with open(val_result_file_path, 'r') as f:
-                val_acc = float(f.read())
-        else:
-            val_acc = -1.0
-        result['val_accuracy'] = val_acc
-
-        df = df.append(result, ignore_index=True)
+            df = df.append(result, ignore_index=True)
+        except Exception as e:
+            print(f"\n===> something unexpected went wrong!\n"
+                  f"       instance: {instance}\n"
+                  f"       error: {e}\n")
 
     with open(args.output, 'wb') as f:
         pickle.dump(df, f)
