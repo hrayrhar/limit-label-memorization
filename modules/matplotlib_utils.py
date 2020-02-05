98e53fcec5add9a91dd5c072fd0b07f31903793e
Hrayr Harutyunyan <harhro@gmail.com>
2020-02-04 18:21:16 -0800

    Add paired matplotlib colors

--- modules/matplotlib_utils.py
+++ modules/matplotlib_utils.py
@@ -1,3 +1,9 @@
+paired_colors  = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a',
+                  '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94',
+                  '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d',
+                  '#17becf', '#9edae5']
+
+
 def set_default_configs(plt, seaborn=None):
     # assuming in the figsize height = 5
     plt.rcParams['lines.linewidth'] = 1.5
