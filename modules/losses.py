ff3a98136ee646da76723416d264875844412114
Hrayr Harutyunyan <harhro@gmail.com>
2020-04-10 00:01:39 -0700

    Use losses available in nnlib

--- modules/losses.py
+++ modules/losses.py
@@ -1,30 +1,11 @@
 import torch
 import torch.nn.functional as F
+import nnlib.nnlib.losses
 
 
-def binary_cross_entropy(target, pred):
-    target = target.reshape((target.shape[0], -1))
-    pred = pred.reshape((pred.shape[0], -1))
-    ce = F.binary_cross_entropy(pred, target, reduction='none')
-    ce = torch.sum(ce, dim=1)
-    ce = torch.mean(ce, dim=0)
-    return ce
-
-
-def mse(target, pred):
-    target = target.reshape((target.shape[0], -1))
-    pred = pred.reshape((pred.shape[0], -1))
-    mse = torch.sum((target - pred) ** 2, dim=1)
-    mse = torch.mean(mse, dim=0)
-    return mse
-
-
-def mae(target, pred):
-    target = target.reshape((target.shape[0], -1))
-    pred = pred.reshape((pred.shape[0], -1))
-    mad = torch.sum(torch.abs(target - pred), dim=1)
-    mad = torch.mean(mad, dim=0)
-    return mad
+# import some loss functions from nnlib
+mse = nnlib.nnlib.losses.mse
+mae = nnlib.nnlib.losses.mae
 
 
 def gce(target, pred, q=1.0):
